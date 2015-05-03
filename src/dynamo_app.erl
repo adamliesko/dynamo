a630c8f560674a36eacafef85489a9214c34920b
adamliesko <adamliesko@gmail.com>
2015-05-03 17:29:18 +0200

    setup routing

--- src/dynamo_app.erl
+++ src/dynamo_app.erl
@@ -5,7 +5,16 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
+    Dispatch = cowboy_router:compile([
+        {'_', [{"/get", get_handler, []},{"/put", put_handler, []},{"/post", post_handler, []},{"/stats", stats_handler, []},
+        {"/nodes", nodes_handler, []}]}
+    ]),
+    {ok, _} = cowboy:start_http(my_http_listener, 100, [{port, 8080}],
+        [{env, [{dispatch, Dispatch}]}]
+    ),
 	dynamo_sup:start_link().
-
 stop(_State) ->
 	ok.
+
+
+
