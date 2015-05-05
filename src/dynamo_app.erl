12237b709ef1a30fb13949632b388f4047f5ec1d
adamliesko <adamliesko@gmail.com>
2015-05-05 23:15:20 +0200

    Create root_handler, delete rest

--- src/dynamo_app.erl
+++ src/dynamo_app.erl
@@ -5,14 +5,19 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
+	lists:foreach(fun(_) ->
+		   kvs_server:start_link()
+		  end, lists:seq(0, 10)),
+    
     Dispatch = cowboy_router:compile([
-        {'_', [{"/get", get_handler, []},{"/put", put_handler, []},{"/post", post_handler, []},{"/", stats_handler, []},
+        {'_', [{"/stats", stats_handler, []},{"/", root_handler, []},
         {"/nodes", nodes_handler, []}]}
     ]),
     {ok, _} = cowboy:start_http(my_http_listener, 100, [{port, 8080}],
         [{env, [{dispatch, Dispatch}]}]
     ),
 	dynamo_sup:start_link().
+
 stop(_State) ->
 	ok.
 
---
+++ src/root_handler.erl
@@ -0,0 +1,43 @@
+-module(root_handler).
+-behaviour(cowboy_http_handler).
+-include("kvs.hrl").
+
+-export([init/3]).
+-export([handle/2]).
+-export([terminate/3]).
+
+-record(state, {
+}).
+
+init(_, Req, _Opts) ->
+	{ok, Req, #state{}}.
+
+handle(Req, State=#state{}) ->
+	{Method, Req2} = cowboy_req:method(Req),
+
+	
+	Params = cowboy_req:match_qs([val,key], Req),
+
+	#{key := Key} = Params,
+	#{val := Val} = Params,
+
+    case Method of
+        <<"POST">> ->
+        	Pid = pg2:get_closest_pid(ring),
+    		Res = gen_server:call(Pid, {set, Key, Val}, 1),
+          
+            {ok, Req3} = cowboy_req:reply(200, [], Res, Req2),
+            {ok, Req3, State};
+        <<"GET">> ->
+        	Pid = pg2:get_closest_pid(ring),
+    		Res = gen_server:call(Pid, {get, Key}, 1),
+
+            {ok, Req3} = cowboy_req:reply(200, [], Res, Req2),
+            {ok, Req3, State};
+         _ ->
+         	{ok, Req3} = cowboy_req:reply(200, [], 'else', Req2),
+         	{ok, Req3, State}
+    end.
+
+terminate(_Reason, _Req, _State) ->
+	ok.
