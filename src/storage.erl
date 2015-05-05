fafbf86886fb88a63cb87bd0cb895ca586a1d655
Barbora Brockova <barbora.brockova@gmail.com>
2015-05-06 00:06:08 +0200

    Add put operation

--- src/storage.erl
+++ src/storage.erl
@@ -1,8 +1,8 @@
 -module(storage).
 -behaviour(gen_server).
 
--export([start_link/3, get/2,close/1]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, code_change/3, terminate/2]).
+-export([start_link/3, get/2, put/3, close/1]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, code_change/3, terminate/2,]).
 
 -record(storage, {module,table_storage,name}).
 
@@ -12,6 +12,8 @@ start_link(Storehouse, IdKey, Name) ->
 get(Name, Key) ->
 	gen_server:call(Name, {get, Key}).
 
+put(Name, Key, Val) ->
+	gen_server:call(Name, {put, Key, Val}).
 
 close(Name) ->
     gen_server:call(Name, close).
@@ -33,6 +35,9 @@ code_change(_Old, State, _) ->
 
 handle_call({get, Key}, _From, State = #storage{module=Module,table_storage=TableStorage}) ->
 	{reply, Module:get(Key, TableStorage), State};
+
+handle_call({put, Key, Val}, _From, State = #storage{module=Module,table_storage=TableStorage}) ->
+	{reply, ok, State#storage{table_storage=Module:put(Key,Val,TableStorage)}};
 
 handle_call(close, _From, State) ->
 	{stop, close, ok, State}.
