b5eef317b1b36600d605a55806fb1500a7f5a474
raethlo <roman.rostar@gmail.com>
2015-05-09 13:43:19 +0200

    Implement join operation (aka merge) in vector clock

--- src/vector_clock.erl
+++ src/vector_clock.erl
@@ -3,18 +3,58 @@
 
 new(Node) -> [{Node, 1}].
 
-fix({_FClock, _FValues} = First, {_SClock, _SValues} = Second) ->
-  ComparisonResult = diff(First,Second),
+fix({FClock, _FValues} = First, {SClock, _SValues} = Second) ->
+  ComparisonResult = diff(FClock,SClock),
   case ComparisonResult of
       leq ->
-          Second;
+        Second;
       geq ->
         First;
       eq ->
         First;
       _ ->
-        join(First,Second)
+        join(FClock,SClock, [])
   end.
+
+%% to be checked
+
+%%keytake(Key, N, TupleList1) -> {value, Tuple, TupleList2} | false
+%%  Searches the list of tuples TupleList1 for a tuple whose Nth element
+%%  compares equal to Key. %% Returns {value, Tuple, TupleList2} if such a tuple
+%%  is found, otherwise false. TupleList2 is a copy of TupleList1 where the
+%%  first occurrence of Tuple has been removed.
+
+%%keymerge(N, TupleList1, TupleList2) -> TupleList3
+%%  Returns the sorted list formed by merging TupleList1 and TupleList2. The
+%%  merge is performed on the Nth element of each tuple. Both TupleList1 and
+%%  TupleList2 must be key-sorted prior to evaluating this function. When two
+%%  tuples compare equal, the tuple from TupleList1 is picked before the tuple
+%%  from TupleList2.
+
+%%keysort(N, TupleList1) -> TupleList2
+%%  Returns a list containing the sorted elements of the list TupleList1
+%%  Sorting is performed on the Nth element of the tuples. The sort is stable.
+%%
+
+join([{FirstNode, VersionFirst}|First], Second,Acc) ->
+%% simple pattern matching, check what keytake returns - it remove the element
+  case lists:keytake(FirstNode, 1, Second) of
+    {value, {FirstNode, VersionSecond}, SecondRest} when VersionFirst > VersionSecond ->
+      join(First,SecondRest,[{First,VersionFirst}|Acc]);
+    {value, {FirstNode, VersionSecond}, SecondRest}  when VersionSecond >= VersionFirst ->
+      join(First,SecondRest,[{First,VersionSecond}|Acc]);
+    _ ->
+      join(First,Second,[{First,VersionFirst}|Acc])
+  end;
+
+join([], Second, Acc) ->
+  SortedSecond = lists:keysort(1, Second),
+  lists:keymerge(1, Acc, SortedSecond);
+
+join(First, [], Acc) ->
+  SortedFirst = lists:keysort(1, First),
+  lists:keymerge(1, Acc, SortedFirst).
+
 
 %% todo
 diff(First, Second) ->
@@ -22,13 +62,7 @@ diff(First, Second) ->
   Second,
   eq.
 
-%% todo
-join(First,Second) ->
-  First,
-  Second,
-  First.
-
-%% increase context value for each
+%% increase context value for each - to be checked
 incr(Node, [Context|VectorClocks]) ->
   UpdatedClocks =   [{CurrentNode, CurrentContext + 1} || {CurrentNode,CurrentContext} <- VectorClocks],
   FinalClocks = [UpdatedClocks | {Node,1}],
